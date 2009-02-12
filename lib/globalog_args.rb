17f5a888c13e479d9f71c7ebaae723c47c71b550
lp <lp@spiralix.org>
2009-02-12 11:59:08 -0500

    for Globalog::Args

---
+++ lib/globalog_args.rb
@@ -0,0 +1,43 @@
+class GlobaLog
+	# Author:: lp (mailto:lp@spiralix.org)
+	# Copyright:: 2009 Louis-Philippe Perron - Released under the terms of the MIT license
+	# 
+	# :title:GlobaLog/Args
+	
+	module Args
+		
+		def Args.are(args)
+			$logger_args[:log_level] ||= args[:log_level].to_sym
+			$logger_args[:log_output] ||= args[:log_output]
+		end
+		
+		def Args::log_output
+			$logger_args[:log_output]
+		end
+
+		def Args::log_level
+			if $logger_args[:log_level].is_a?(Symbol)
+				$logger_args[:log_level] = self.sym_to_level($logger_args[:log_level])
+			end
+		end
+		
+		def Args.sym_to_level(sym)
+			case sym
+			when :debug
+				return Logger::DEBUG
+			when :info
+				return Logger::INFO
+			when :warn
+				return Logger::WARN
+			when :error
+				return Logger::ERROR
+			when :fatal
+				return Logger::FATAL
+			else
+				return Logger::UNKNOWN
+			end
+		end
+		
+	end
+	
+end
