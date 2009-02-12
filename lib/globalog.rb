5b40fe682a5adb48ab5d9aafb141b9ab2eb46602
lp <lp@spiralix.org>
2009-02-12 11:58:33 -0500

    splitted in multiple files

--- lib/globalog.rb
+++ lib/globalog.rb
@@ -60,6 +60,8 @@
 
 require 'logger'
 class GlobaLog < Logger
+	require 'globalog_args'
+	require 'globalog_hijack'
 	$logger_args ||= Hash.new
 	
 	def GlobaLog.logger(def_out,def_level,follow=true)
@@ -80,85 +82,6 @@ class GlobaLog < Logger
 	
 	def GlobaLog::level
 		Args::log_level
-	end
-	
-	module Args
-		
-		def Args.are(args)
-			$logger_args[:log_level] ||= args[:log_level].to_sym
-			$logger_args[:log_output] ||= args[:log_output]
-		end
-		
-		def Args::log_output
-			$logger_args[:log_output]
-		end
-
-		def Args::log_level
-			if $logger_args[:log_level].is_a?(Symbol)
-				$logger_args[:log_level] = self.sym_to_level($logger_args[:log_level])
-			end
-		end
-		
-		def Args.sym_to_level(sym)
-			case sym
-			when :debug
-				return Logger::DEBUG
-			when :info
-				return Logger::INFO
-			when :warn
-				return Logger::WARN
-			when :error
-				return Logger::ERROR
-			when :fatal
-				return Logger::FATAL
-			else
-				return Logger::UNKNOWN
-			end
-		end
-		
-	end
-	
-	class OptHijacker < Hash
-		require 'optparse'
-		
-		def initialize
-			super()
-			self[:log_level] = nil
-			self[:log_output] = nil
-			opts = OptionParser.new do |opts|
-				opts.on('-L','--log-level [STRING]','sets the Test Log Level') do |string|
-					self[:log_level] = string.to_sym
-				end
-				opts.on('-O','--log-output [I/O]','sets the Test Log output') do |io|
-					self[:log_output] = io
-				end
-			end
-			opts.parse!($keep_argv)
-		end
-
-		def self.keep_wanted_argv
-			args = Array.new(ARGV); ARGV.clear; keep = []
-			wanted = ['-L', '--log-level', '-O', '--log-output']
-			catch :finished do
-				loop do
-					throw :finished if args.empty?
-					catch :found do
-						wanted.each do |arg|
-							if args[0] == arg
-								2.times { keep << args.shift }
-								throw :found
-							end
-						end
-						ARGV << args.shift
-					end
-				end
-			end
-			$keep_argv ||= keep
-		end
-
-		self.keep_wanted_argv
-		Args.are(self.new)
-
 	end
 	
 end
