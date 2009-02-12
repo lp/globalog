f93e898987c8815744c895afeed807f25e9f24a0
lp <lp@spiralix.org>
2009-02-12 11:59:58 -0500

    for Globalog::Hijack

---
+++ lib/globalog_hijack.rb
@@ -0,0 +1,50 @@
+class GlobaLog
+	# Author:: lp (mailto:lp@spiralix.org)
+	# Copyright:: 2009 Louis-Philippe Perron - Released under the terms of the MIT license
+	# 
+	# :title:GlobaLog/Hijack
+	
+	class Hijack < Hash
+		require 'optparse'
+		
+		def initialize
+			super()
+			self[:log_level] = nil
+			self[:log_output] = nil
+			opts = OptionParser.new do |opts|
+				opts.on('-L','--log-level [STRING]','sets the Test Log Level') do |string|
+					self[:log_level] = string.to_sym
+				end
+				opts.on('-O','--log-output [I/O]','sets the Test Log output') do |io|
+					self[:log_output] = io
+				end
+			end
+			opts.parse!($keep_argv)
+		end
+
+		def self.keep_wanted_argv
+			args = Array.new(ARGV); ARGV.clear; keep = []
+			wanted = ['-L', '--log-level', '-O', '--log-output']
+			catch :finished do
+				loop do
+					throw :finished if args.empty?
+					catch :found do
+						wanted.each do |arg|
+							if args[0] == arg
+								2.times { keep << args.shift }
+								throw :found
+							end
+						end
+						ARGV << args.shift
+					end
+				end
+			end
+			$keep_argv ||= keep
+		end
+
+		self.keep_wanted_argv
+		Args.are(self.new)
+
+	end
+	
+end
