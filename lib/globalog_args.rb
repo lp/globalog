d60e96cf7ad767c45a54368889fe4a59b571bafa
lp <lp@spiralix.org>
2009-02-14 11:34:17 -0500

    :log_opts implemented

--- lib/globalog_args.rb
+++ lib/globalog_args.rb
@@ -11,9 +11,11 @@ class GlobaLog
 			if override == true
 				$logger_args[:log_level] = args[:log_level].to_sym unless args[:log_level].nil?
 				$logger_args[:log_output] = args[:log_output] unless args[:log_output].nil?
+				$logger_args[:log_opts] = args[:log_opts] unless args[:log_opts].nil?
 			else
 				$logger_args[:log_level] ||= args[:log_level].to_sym unless args[:log_level].nil?
 				$logger_args[:log_output] ||= args[:log_output] unless args[:log_output].nil?
+				$logger_args[:log_opts] ||= args[:log_opts] unless args[:log_opts].nil?
 			end
 		end
 		
@@ -26,6 +28,10 @@ class GlobaLog
 				$logger_args[:log_level] = self.sym_to_level($logger_args[:log_level])
 			end
 			$logger_args[:log_level]
+		end
+		
+		def Args::log_opts
+			$logger_args[:log_opts]
 		end
 		
 		def Args.sym_to_level(sym)
