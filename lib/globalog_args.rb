6b64f7d7e9569cff30e8b83b1e6bd6d5ba52d131
lp <lp@spiralix.org>
2009-02-28 12:21:15 -0500

    opts flag to take precedence when command line opts are present

--- lib/globalog_args.rb
+++ lib/globalog_args.rb
@@ -7,8 +7,8 @@ class GlobaLog
 	module Args
 		
 		def Args.are(args,override=false)
-			$logger_args ||= Hash.new
-			if override == true
+			$logger_args ||= Hash.new; $logger_args[:opts] = true unless args[:opts].nil?
+			if $logger_args[:opts].nil? and override == true
 				$logger_args[:log_level] = args[:log_level].to_sym unless args[:log_level].nil?
 				$logger_args[:log_output] = args[:log_output] unless args[:log_output].nil?
 				$logger_args[:log_opts] = args[:log_opts] unless args[:log_opts].nil?
